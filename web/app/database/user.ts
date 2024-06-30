284062b0528e3cec21543487d5594e15bb1dce4d
KoharaKazuya <reflect.results@gmail.com>
2024-06-30 13:00:47 +0900

    fix: 要約 AI 設定画面を開いたときにエラーになることがある問題を修正する

--- web/app/database/user.ts
+++ web/app/database/user.ts
@@ -10,7 +10,7 @@ export async function getPrompt({
   const docRef = doc(firestore, "users", userId);
   const docSnap = await getDoc(docRef);
   if (!docSnap.exists()) return "";
-  return docSnap.data().prompt;
+  return docSnap.data().prompt ?? "";
 }
 
 /** プロンプトを更新する */
